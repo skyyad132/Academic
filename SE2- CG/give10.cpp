53178877a1bab912a0d279a8b531aa407bab865d
Satyam Dahiwal <32240792+Scresat@users.noreply.github.com>
2019-04-22 20:14:25 +0530

    change

--- SE2- CG/give10.cpp
+++ SE2- CG/give10.cpp
@@ -1,144 +1,142 @@
-#include<iostream>
-#include<GL/glut.h>
-using namespace std;
- 
-int frame1=0;
- 
-void Init()
-{
-	glClearColor(0.6,0.9,1,0);
-	gluOrtho2D(0,640,0,480);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glMatrixMode(GL_MODELVIEW);
-}
- 
-void car()
-{
-	glColor3f(0.8,0.8,0.8);
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <math.h>
+
+const double PI = 3.141592654;
+int frameNumber = 0;
+
+void drawDisk(double radius) {
+	int d;
 	glBegin(GL_POLYGON);
-	glVertex2d(20,160);
-	glVertex2d(20,200);
-	glVertex2d(100,200);
-	glVertex2d(120,180);
-	glVertex2d(120,160);
+	for (d = 0; d < 32; d++) {
+		double angle = 2*PI/32 * d;
+		glVertex2d( radius*cos(angle), radius*sin(angle));
+	}
 	glEnd();
-	
-	glColor3f(0,0,0);
-	glBegin(GL_LINE_LOOP);
-	glVertex2d(80,200);
-	glVertex2d(80,180);
-	glVertex2d(120,180);
-	glVertex2d(100,200);
-	glEnd();
-	
-	glBegin(GL_LINE_LOOP);
-	glVertex2d(20,160);
-	glVertex2d(20,200);
-	glVertex2d(100,200);
-	glVertex2d(120,180);
-	glVertex2d(120,160);
-	glEnd();
-	
-	glColor3f(0,0,0);		//wheels
-	glPushMatrix();
-	glTranslated(30,160,0);
-	glutWireSphere(5,110,110);
-	glPopMatrix();
-	glPushMatrix();
-	glTranslated(110,160,0);
-	glutWireSphere(5,110,110);
-	glPopMatrix();
-	
-	glFlush();
-}
- 
-void doframe(int v)
-{
-	frame1++;
-	glutPostRedisplay();
-	glutTimerFunc(20,doframe,0);
-}
- 
-void Display()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-	glColor3f(0,0.75,0);
-	glBegin(GL_TRIANGLES);	//mountains
-	glVertex2d(0,300);
-	glVertex2d(75,350);
-	glVertex2d(150,300);
-	
-	glVertex2d(140,300);
-	glVertex2d(230,350);
-	glVertex2d(300,300);
-	
-	glVertex2d(340,300);
-	glVertex2d(415,350);
-	glVertex2d(490,300);
-	
-	glVertex2d(480,300);
-	glVertex2d(570,350);
-	glVertex2d(640,300);
-	glEnd();
-	
-	glColor3f(0.5,1,0.5);	//surface
-	glBegin(GL_POLYGON);
-	glVertex2d(0,0);
-	glVertex2d(0,300);
-	glVertex2d(640,300);
-	glVertex2d(640,0);
-	glEnd();
-	
-	glColor3f(0,1,1);		//river
-	glBegin(GL_POLYGON);
-	glVertex2d(220,0);
-	glVertex2d(300,300);
-	glVertex2d(340,300);
-	glVertex2d(400,0);
-	glEnd();
-		
-	glColor3f(0.5,0.5,0.5);	//road
-	glBegin(GL_POLYGON);
-	glVertex2d(0,120);
-	glVertex2d(0,160);
-	glVertex2d(640,160);
-	glVertex2d(640,120);
-	glEnd();
-		
-	glColor3f(1,0,0.5);		//Building
-	glBegin(GL_POLYGON);
-	glVertex2d(10,160);
-	glVertex2d(10,260);
-	glVertex2d(45,300);
-	glVertex2d(80,260);
-	glVertex2d(80,160);
-	glEnd();
-	glColor3f(1,1,1);
-	glBegin(GL_LINES);
-	glVertex2d(45,220);
-	glVertex2d(45,240);
-	glEnd();
-	
-	glPushMatrix();    //Move car from left to right 
-	glTranslated((frame1 % 640),0,0);
-	car();
-	glPopMatrix();
-	glFlush();
-	
-}
-int main(int argc,char **argv)
-{
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(0,0);
-	glutInitWindowSize(640,480);
-	
-	glutCreateWindow("ANIMATION");
-	Init();
-	glutDisplayFunc(Display);
-	glutTimerFunc(100,doframe,0);
-	glutMainLoop();
 }
 
+void drawship() {
+	glColor3f(1.5,1,0);
+	glBegin(GL_POLYGON);
+	glVertex2f(1.0f,5);//x1
+	glVertex2f(-2.0f,4.5);//x2
+	glVertex2f(1.0f,4);//y2
+	glEnd();
+	
+	glColor3f(1.5,0,1);
+	glBegin(GL_LINES);
+	glVertex2f(1.0f,2);//x1
+	glVertex2f(1.0f,4);//y2
+	glEnd();
+	
+	glColor3f(1,0,0);
+	glBegin(GL_POLYGON);
+	glVertex2f(-2.0f,0);//x1
+	glVertex2f(2.0f,0);//x2
+	glVertex2f(3.0f,2);//y2
+	glVertex2f(-3.0f,2);//y1
+	
+	glEnd();
+}
+
+
+void drawSun() {
+	int i;
+	glColor3f(1.0,0.8,0.0);
+	for (i = 0; i < 20; i++) { 
+		glRotatef( 360 / 20, 0, 0, 1 ); 
+		glBegin(GL_LINES);
+		glVertex2f(0, 0);
+		glVertex2f(0.75f, 0);
+		glEnd();
+	}
+	drawDisk(0.5);
+	glColor3f(0,0,0);
+}
+
+void display() {
+
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glLoadIdentity();
+
+	
+
+	glColor3f(0, 0.6f, 0.2f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-3,-1);
+	glVertex2f(1.5f,1.65f);
+	glVertex2f(5,-1);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+	glVertex2f(-3,-1);
+	glVertex2f(3,2.1f);
+	glVertex2f(7,-1);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex2f(0,-1);
+	glVertex2f(6,1.2f);
+	glVertex2f(20,-1);
+	glEnd();
+
+	
+
+	glColor3f(0.2f, 0.2f, 1.0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(0,-0.4f);
+	glVertex2f(7,-0.4f);
+	glVertex2f(7,0.4f);
+	glVertex2f(0,0.4f);
+	glEnd();
+
+	
+	
+	glPushMatrix();
+	glTranslated(1.8,3,0);
+	glRotated(-frameNumber*2.7,0,0,1);
+	drawSun();
+	glPopMatrix();
+
+	
+	glPushMatrix();
+	
+	glTranslated(-3 + 15*(frameNumber % 300) / 300.0, 0, 0);
+	glScaled(0.3,0.3,1);
+	
+	drawship();
+	glPopMatrix();
+	
+	glutSwapBuffers();
+
+}
+
+void doFrame(int v) {
+    frameNumber++;
+    glutPostRedisplay();
+    glutTimerFunc(30,doFrame,0);
+}
+
+void init() {
+	glClearColor(0.6f, 0.6f, 1.0f, 0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 7, -1, 4, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv); 
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(700,500);
+    glutInitWindowPosition(100,100);  
+    glutCreateWindow("Animation"); 
+
+    init();
+
+    glutDisplayFunc(display);   
+    glutTimerFunc(200,doFrame,0); 
+
+    glutMainLoop(); 
+    return 0;
+}    
